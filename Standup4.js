fa4eb43987441f2c343169fb1092e711f86ff0b0
Angela Hobson <angelalhobson@yahoo.com>
2022-10-20 16:19:20 -0600

    Arrays

---
+++ Standup4.js
@@ -0,0 +1,13 @@
+function getAverage(arr) {
+  return Math.round(arr.reduce((a, b) => a + b, 0) / arr.length);
+}
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+getAverage([2, 2, 2, 2]); //,2)
+getAverage([1, 2, 3, 4, 5]); //,3);
+getAverage([1, 1, 1, 1, 1, 1, 1, 2]); //,1)
