cb3685762a26fefa07a3e4f0b38e132bb5d6acea
Angela Hobson <angelalhobson@yahoo.com>
2022-12-02 15:32:44 -0700

    Arrays

---
+++ Standup 16.js
@@ -0,0 +1,28 @@
+function missingVowel(str) {
+  if (!str.includes("a")) {
+    return 0;
+  } else if (!str.includes("e")) {
+    return 1;
+  } else if (!str.includes("i")) {
+    return 2;
+  } else if (!str.includes("o")) {
+    return 3;
+  } else if (!str.includes("u")) {
+    return 4;
+  }
+}
+
+missingVowel("John Doe hs seven red pples under his bsket");
+
+// Your job is to figure out the index of which vowel is missing from a given string:
+
+// A has an index of 0,
+// E has an index of 1,
+// I has an index of 2,
+// O has an index of 3,
+// U has an index of 4.
+// Notes: There is no need for string validation and every sentence given will contain all vowles but one. Also, you won't need to worry about capitals.
+
+// Examples
+// "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
+// "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
