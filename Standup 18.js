ad7e81b8d7792a8f9a39ea61e36ede4e08b1f9b2
Angela Hobson <angelalhobson@yahoo.com>
2022-12-06 11:26:24 -0700

    Strings

---
+++ Standup 18.js
@@ -0,0 +1,36 @@
+function countJewels(jewels, stones) {
+  let count = 0;
+  for (let i = 0; i < jewels.length; i++) {
+    for (let j = 0; j < stones.length; j++) {
+      if (jewels[i] === stones[j]) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+}
+
+countJewels("aA", "aAAbbbb");
+countJewels("z", "ZZ");
+
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have.
+//Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+
+// Constraints:
+
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+//How many times do the characters in JEWELS appear in stones?
+//Take first char in J- "a". iterate through stones and increment. repeat
