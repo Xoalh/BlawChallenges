a4e36a58aa04ac917d3e74a3087fa8f443ac200b
Angela Hobson <angelalhobson@yahoo.com>
2022-12-31 10:32:45 -0700

    Hashmap

---
+++ Standup 21.js
@@ -0,0 +1,32 @@
+var majorityElement = function (nums) {
+  let majority = Math.round(nums.length / 2);
+  //   console.log(majority);
+  let map = {};
+  for (let x of nums) {
+    if (!map[x]) {
+      map[x] = 1;
+    } else if (map[x]) {
+      map[x]++;
+    }
+
+    if (map[x] == majority) {
+      return x;
+    }
+  }
+};
+
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+console.log(majorityElement([3, 2, 3]));
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
