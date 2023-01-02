bacd3892ddd11ed9616fe45ee1eb5831a5f8534d
Angela Hobson <angelalhobson@yahoo.com>
2023-01-02 13:25:31 -0700

    hashmap

--- Standup 23.js
+++ Standup 23.js
@@ -5,6 +5,7 @@ function twoSum(arr, total) {
 
   for (let num of arr) {
     let half = total - num;
+    //console.log(half);
     if (map.indexOf(half) !== -1) {
       console.log(map.indexOf(half));
       answer.push([half, num]);
@@ -12,9 +13,22 @@ function twoSum(arr, total) {
       map.push(num);
     }
   }
-  console.log(answer);
+  //console.log(answer);
   console.log(map);
 }
+
+// function twoSum(arr, total) {
+//   let answer = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === total) {
+//         answer.push([arr[i], arr[j]]);
+//       }
+//     }
+//   }
+//   console.log(answer);
+// }
 
 twoSum([1, 2, 2, 3, 4], 4);
 
