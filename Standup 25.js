6ebe3b162b66b5172d42ee486eeb31aafff82ef7
Angela Hobson <angelalhobson@yahoo.com>
2023-01-04 15:54:47 -0700

    Fibonacci - Memoization

---
+++ Standup 25.js
@@ -0,0 +1,16 @@
+let memo = {};
+
+function fib(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  } else if (memo[n]) {
+    return memo[n];
+  } else {
+    let result = fib(n - 1) + fib(n - 2);
+    memo[n] = result;
+    return result;
+  }
+}
+
+console.log(fib(6));
+console.log(memo);
